f7414973d1a0e634743ef1df4aca4c4861c19e7d
haeyonghahn <yong80211@gmail.com>
2025-04-06 13:31:33 +0900

    Docs: 이력서 수정

--- payload/experience.ts
+++ payload/experience.ts
@@ -11,11 +11,9 @@ const experience: IExperience.Payload = {
           title: 'ERP / Software Developer',
           startedAt: '2020-08',
           descriptions: [
-            '내부 그룹웨어 팀 및 외부 협력사와 협업하여 전자결재 시스템 개발',
             '재무 업무 비즈니스에 필요한 주요 API 구현 및 데이터 모델 설계',
+            '내부 그룹웨어 및 고객사 전자결재 연동 시스템 개발',
             '사용자 Needs 에 맞는 사용성 개선과 신규 비즈니스 로직 구현',
-            'Quartz Job 스케줄러와 Spring Batch를 사용한 자동화',
-            '기술 스터디를 통한 팀 내 전문성 및 협업 역량 강화',
           ],
           skillKeywords: [
             'JavaScript',
